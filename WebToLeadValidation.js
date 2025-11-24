efca4fcfe1045134b5671be0a712479565d856bc
nulqqqqq <dlekm1@mail.ru>
2025-11-24 06:18:16 +0300

    add validation

--- WebToLeadValidation.js
+++ WebToLeadValidation.js
@@ -1,3 +1,5 @@
+// WebToLeadValidation.js
+
 function timestamp() { 
     const response = document.getElementById("g-recaptcha-response"); 
     if (response == null || response.value.trim() === "") {
@@ -20,9 +22,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const productSelect = document.getElementById('product_select');
     const productField = document.getElementById('00Ng5000003Yefd');
     const form = document.getElementById('webToLeadForm');
-    const submitBtn = document.getElementById('submitBtn');
-
+    
     if (productSelect && productField) {
+        productField.value = productSelect.value;
+        
         productSelect.addEventListener('change', function() {
             const productCode = this.value;
             productField.value = productCode;
@@ -32,19 +35,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (form) {
         form.addEventListener('submit', function(e) {
+
+            if (!form.checkValidity()) {
+                console.log("Native validation failed.");
+                return;
+            }
             if (!productField || !productField.value) {
                 e.preventDefault();
                 alert('Please select the product');
                 return;
             }
-            
             const recaptchaResponse = grecaptcha.getResponse();
             if (!recaptchaResponse) {
                 e.preventDefault();
                 alert('Please pass the reCAPTCHA check.');
                 return;
             }
-            
+            console.log("All checks have been completed. The form will be sent.");
         });
     }
-}); 
+});
